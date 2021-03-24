00d94df02cd7b159f9dee41fcf1344731c65c54d
Mario Rivis <mario.rivis@gmail.com>
2021-03-24 18:26:05 +0200

    New version 1.1.0, added Env prefix option

--- src/main/kotlin/org/dxworks/argumenthor/config/sources/impl/EnvSource.kt
+++ src/main/kotlin/org/dxworks/argumenthor/config/sources/impl/EnvSource.kt
@@ -4,12 +4,16 @@ import org.dxworks.argumenthor.config.fields.FieldConfig
 import org.dxworks.argumenthor.config.sources.ConfigurationSource
 import org.dxworks.argumenthor.config.sources.ENV
 
-class EnvSource : ConfigurationSource {
+class EnvSource(prefix: String = "") : ConfigurationSource {
+    constructor() : this("")
+
+    private val prefixString: String = if(prefix.isEmpty()) "" else prefix + "_"
+
     override fun <T> get(field: FieldConfig<T>): T? {
         return field.parse(System.getenv(getEnvName(field)))
     }
 
-    private fun <T> getEnvName(field: FieldConfig<T>) = field.name.replace(".", "_").toUpperCase()
+    private fun <T> getEnvName(field: FieldConfig<T>) = prefixString + field.name.replace(".", "_").toUpperCase()
 
     override val name = ENV
 }
